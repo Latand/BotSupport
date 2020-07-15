ee902ceab0a401dd12c2089431760ce5557184d2
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-07-15 20:09:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from loader import bot, storage
-
-
 async def on_startup(dp):
     import filters
     import middlewares
@@ -10,14 +7,8 @@ async def on_startup(dp):
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
 
-
-async def on_shutdown(dp):
-    await bot.close()
-    await storage.close()
-
-
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp, on_startup=on_startup)
