b979719aaf315c6fb2eea26b455aea7d3772d3ee
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-08-05 21:14:07 +0300

    fix

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ async def on_startup(dp):
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
 
+
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
--- data/config.py
+++ data/config.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+
 admins = [
+    os.getenv("ADMIN_ID"),
 ]
 
 ip = os.getenv("ip")
