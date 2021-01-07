3940f090d26b81355ded98cbf969d12e2bd4c6d3
Daniil <comictomcat@gmail.com>
2021-01-07 14:39:38 +0200

    Убрал бардак из разных скобок. Изменил импорты в app.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -4,5 +4,5 @@ from loader import dp
 from .throttling import ThrottlingMiddleware
 
 
-if __name__ == "a.middlewares":
+if __name__ == "middlewares":
     dp.middleware.setup(ThrottlingMiddleware())
